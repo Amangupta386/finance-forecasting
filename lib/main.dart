7e708cdcbe4e26d748e90fa945051f86b6b5911a
myChatApp <amanguptaji386@gmail.com>
2023-07-27 17:28:31 +0530

    working on data tables

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:finance_forecasting/widgets/dataGridTable.dart';
+import 'package:finance_forecasting/widgets/scrollViewBehavior.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scrollBehavior: MyCustomScrollBehavior(),
+      title: 'Flutter Demo',
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //   useMaterial3: true,
+      // ),
+      home: DataTableWidget(),
+    );
+  }
+}
---
+++ lib/widgets/custom_data_cell.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart';
+//
+// class CustomDataCell extends StatelessWidget {
+//   const CustomDataCell(Container container, {Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return CustomDataCell(DataCell(Container(
+//       height: 48,
+//       width: 120,
+//       decoration: BoxDecoration(
+//           border: Border.all(width: 1, color: const Color(0xffF2F2F2)),
+//           borderRadius: BorderRadius.circular(4)),
+//       child: const Padding(
+//         padding: EdgeInsets.only(left: 10, top: 8.3),
+//         child: Text('\$0'),
+//       ),
+//     )) as Container);
+//   }
+// }
