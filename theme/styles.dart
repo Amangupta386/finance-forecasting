83d6585cb8b7b8690a1faf7f23464b878a3239df
Aman Gupta <95014109+Amangupta386@users.noreply.github.com>
2023-11-14 19:11:27 +0530

    Add files via upload

---
+++ theme/styles.dart
@@ -0,0 +1,114 @@
+///Font sizes used across the application.
+class FontSizes {
+  static double get scale => 1;
+  static double get s7 => 7 * scale;
+  static double get s8 => 8 * scale;
+  static double get s10 => 10 * scale;
+  static double get s11 => 11 * scale;
+  static double get s12 => 12 * scale;
+  static double get s14 => 14 * scale;
+  static double get s16 => 16 * scale;
+  static double get s18 => 18 * scale;
+  static double get s20 => 20 * scale;
+  static double get s24 => 24 * scale;
+  static double get s28 => 28 * scale;
+  static double get s35 => 35 * scale;
+  static double get s48 => 48 * scale;
+}
+
+///Insets used across the application.
+class Insets{
+  static double get scale => 1;
+  static double get s1 => 1 * scale;
+  static double get s2 => 2 * scale;
+  static double get s3 => 3 * scale;
+  static double get s2d8 => 2.8 * scale;
+  static double get s4 => 4 * scale;
+  static double get s5 => 5 * scale;
+  static double get s7 => 7 * scale;
+  static double get s8 => 8 * scale;
+  static double get s08 => 8 * scale;
+  static double get s10 => 10 * scale;
+  static double get s11 => 11 * scale;
+  static double get s12 => 12 * scale;
+  static double get s14 => 14 * scale;
+  static double get s15 => 15 * scale;
+  static double get s15d5 => 15.5 * scale;
+  static double get s16 => 16 * scale;
+  static double get s20 => 20 * scale;
+  static double get s24 => 24 * scale;
+  static double get s25 => 25 * scale;
+  static double get s30 => 30 * scale;
+  static double get s40 => 40 * scale;
+  static double get s48 => 48 * scale;
+  static double get s18 => 18 * scale;
+  static double get s28 => 28 * scale;
+}
+
+///Sizes used across the application.
+class Sizes{
+  static double get scale => 1;
+  static double get s05 => 5 * scale;
+  static double get s08 => 8 * scale;
+  static double get s10 => 10 * scale;
+  static double get s11 => 11 * scale;
+  static double get s12 => 12 * scale;
+  static double get s14 => 14 * scale;
+  static double get s16 => 16 * scale;
+  static double get s20 => 20 * scale;
+  static double get s24 => 24 * scale;
+  static double get s30 => 30 * scale;
+  static double get s40 => 40 * scale;
+  static double get s48 => 48 * scale;
+  static double get s18 => 18 * scale;
+  static double get s28 => 28 * scale;
+  static double get s300 => 300 * scale;
+}
+
+///Icon sizes used across the application.
+class IconSizes {
+  static const double scale = 1;
+  static double get s10 => 10 * scale;
+  static double get s11 => 11 * scale;
+  static double get s12 => 12 * scale;
+  static double get s14 => 14 * scale;
+  static double get s15 => 15 * scale;
+  static double get s16 => 16 * scale;
+  static double get s17 => 17 * scale;
+  static double get s18 => 18 * scale;
+  static const double s24 = 24 * scale;
+}
+
+///Media query sizes used across the application.
+class MediaQuerySizes{
+  static double get scale => 1;
+  static double get s2 => 2 * scale;
+  static double get s4 => 4 * scale;
+  static double get s8 => 8 * scale;
+  static double get s25 => 25 * scale;
+  static double get s40 => 40 * scale;
+  static double get s80 => 80 * scale;
+  static double get s89 => 89 * scale;
+  //decimals
+  static double get s7d7 => 7.7 * scale;
+  static double get s42d5 => 42.5 * scale;
+  static double get s6d1 => 6.1 * scale;
+
+}
+
+///Divider sizes used across the application.
+class DividerSizes{
+  static double get scale => 1;
+  static double get s2 => 10 * scale;
+}
+
+///Position sizes used across the application.
+class PositionSizes{
+  static double get scale => 1;
+  static double get s20 => 20 * scale;
+  static double get s31 => 31 * scale;
+  static double get s45 => 45 * scale;
+  static double get s98 => 98 * scale;
+  static double get s120 => 120 * scale;
+  static double get s118 => 118 * scale;
+}
