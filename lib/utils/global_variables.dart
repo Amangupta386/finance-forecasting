02c68646bf7b4172b3c4da0be4ea88087b7877c1
Aman Gupta <95014109+Amangupta386@users.noreply.github.com>
2023-12-08 15:30:19 +0530

    Delete lib directory

--- lib/utils/global_variables.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-final TextEditingController searchController = TextEditingController();
-final GlobalKey<NavigatorState> rootNavigatorKey =GlobalKey<NavigatorState>(debugLabel: 'root');
--- lib/utils/models.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class LoginControllers {
-  final TextEditingController email = TextEditingController();
-  final TextEditingController password = TextEditingController();
-}
